3d1b87ab9ae652a0a42636ea44b58a79a009b7d1
Diwas777 <poudel.diwas789@gmail.com>
2017-12-08 11:48:51 +0545

    create layout/master.blade.php

---
+++ resources/views/todo/todo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>List</title>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			@section('body')
+
+			@endsection
+		</div>
+	</div>
+	
+</body>
+</html>
